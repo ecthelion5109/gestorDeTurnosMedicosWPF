722aeb0b5ad8529a5025e3420164fc6bbaf25687
Ayelen <aye_pereira77@outlook.com>
2024-10-05 14:53:14 -0300

    se creo gestion de paciente

--- Pacientes.xaml.cs
+++ Pacientes.xaml.cs
@@ -20,5 +20,10 @@ namespace ClinicaMedica {
 		public Pacientes() {
 			InitializeComponent();
 		}
-	}
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
 }
