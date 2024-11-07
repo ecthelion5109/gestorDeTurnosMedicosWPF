cc5572d9e7ac11e894b6412e171f6d361c83bf99
Justo <justosanchezprogramacion@gmail.com>
2024-11-07 00:53:55 -0300

    probando

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,47 +7,84 @@ using System.IO;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Data.SqlClient;
+using System.Media;
+using System;
+using System.Windows.Media;
 
 
-namespace ClinicaMedica {
-	public partial class App : Application {
-		public static bool UsuarioLogueado = false;
-		public static string UsuarioName = "Señor Gestor";
-		public static IBaseDeDatos BaseDeDatos;
-	}
-    public static class WindowExtensions{
-		public static void NavegarA<T>(this Window previousWindow) where T : Window, new(){
-			T nuevaVentana = new();
-			Application.Current.MainWindow = nuevaVentana;  // Establecer la nueva ventana como la principal
-			nuevaVentana.Show();  // Mostrar la nueva ventana
-			previousWindow.Close();  // Cerrar la ventana actual
-		}
-		public static void NavegarA<T>(this Window previousWindow, object optionalArg) where T : Window, new(){
-			T nuevaVentana = (T)Activator.CreateInstance(typeof(T), optionalArg);
-			Application.Current.MainWindow = nuevaVentana;  // Establecer la nueva ventana como la principal
-			nuevaVentana.Show();  // Mostrar la nueva ventana
-			previousWindow.Close();  // Cerrar la ventana actual
-		}
-		
-		public static void AbrirComoDialogo<T>(this Window previousWindow) where T : Window, new(){
-			T nuevaVentana = new();
-			Application.Current.MainWindow = nuevaVentana;
-			nuevaVentana.ShowDialog();
-		}
+namespace ClinicaMedica
+{
+    public partial class App : Application
+    {
+        public static bool UsuarioLogueado = false;
+        public static string UsuarioName = "Señor Gestor";
+        public static IBaseDeDatos BaseDeDatos;
 
-		public static void AbrirComoDialogo<T>(this Window previousWindow, object optionalArg) where T : Window{
-			// Utiliza Activator para instanciar la ventana con el parámetro opcional
-			T nuevaVentana = (T)Activator.CreateInstance(typeof(T), optionalArg);
-			Application.Current.MainWindow = nuevaVentana;
-			nuevaVentana.ShowDialog();
-		}
-		
-		public static void VolverAHome(this Window previousWindow){
-			previousWindow.NavegarA<MainWindow>();
-		}
-		public static void Salir(this Window previousWindow){
-			Application.Current.Shutdown();  // Apagar la aplicación
-		}
-	}
+        // Agregamos las instancias de SoundPlayer para que se carguen una vez al iniciar la app
+        public static  SoundPlayer? HoverSound;
+        public static  SoundPlayer? ClickSound;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Cargamos los sonidos
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri("sonidos\\uclicknofun.wav", UriKind.Relative));
+            mediaPlayer.Play();
+        }
+    }
+
+    public static class WindowExtensions
+    {
+        public static void NavegarA<T>(this Window previousWindow) where T : Window, new()
+        {
+            T nuevaVentana = new();
+            Application.Current.MainWindow = nuevaVentana;
+            nuevaVentana.Show();
+            previousWindow.Close();
+        }
+
+        public static void NavegarA<T>(this Window previousWindow, object optionalArg) where T : Window, new()
+        {
+            T nuevaVentana = (T)Activator.CreateInstance(typeof(T), optionalArg);
+            Application.Current.MainWindow = nuevaVentana;
+            nuevaVentana.Show();
+            previousWindow.Close();
+        }
+
+        public static void AbrirComoDialogo<T>(this Window previousWindow) where T : Window, new()
+        {
+            T nuevaVentana = new();
+            Application.Current.MainWindow = nuevaVentana;
+            nuevaVentana.ShowDialog();
+        }
+
+        public static void AbrirComoDialogo<T>(this Window previousWindow, object optionalArg) where T : Window
+        {
+            T nuevaVentana = (T)Activator.CreateInstance(typeof(T), optionalArg);
+            Application.Current.MainWindow = nuevaVentana;
+            nuevaVentana.ShowDialog();
+        }
+
+        public static void VolverAHome(this Window previousWindow)
+        {
+            previousWindow.NavegarA<MainWindow>();
+        }
+
+        public static void Salir(this Window previousWindow)
+        {
+            Application.Current.Shutdown();
+        }
+
+        public static void Button_MouseEnter(object sender, System.Windows.RoutedEventArgs e)
+        {
+            App.HoverSound?.Play();
+        }
+
+        public static void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            App.ClickSound?.Play();
+        }
+    }
 }
