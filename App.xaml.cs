a51caa4d5ba2e9d02f01b05331e333b91f6d2533
ecthelion5109 <xanderseling@gmail.com>
2024-11-08 05:32:57 -0300

    logica de sonidos acomodada.

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,27 +6,22 @@ namespace ClinicaMedica {
 	public partial class App : Application {
 		public static BaseDeDatosAbstracta BaseDeDatos;
 		public static bool UsuarioLogueado = false;
-		// public static SoundPlayer UClick = new SoundPlayer("sonidos\\uclick.wav");
-        // public static SoundPlayer UClickNoFun = new SoundPlayer("sonidos\\uclicknofun.wav");
-		// public static SoundPlayer UClickJewel = new SoundPlayer("sonidos\\uclick_jewel.wav");
-		public static MediaPlayer UClick = new MediaPlayer();
-		public static MediaPlayer UClickNoFun = new MediaPlayer();
-		public static MediaPlayer UClickJewel = new MediaPlayer();
+		public static MediaPlayer Sonidito = new MediaPlayer();
+
+		// public void StyleButton_Click(object sender, System.Windows.RoutedEventArgs e) {
+			// Sonidito.Open(new Uri("sonidos\\uclick_jewel.wav", UriKind.Relative));
+			// Sonidito.Play();
+		// }
 
 		public void StyleButton_MouseEnter(object sender, System.Windows.RoutedEventArgs e){
-			// UClickJewel.Play();
-		}
-
-		public void StyleButton_Click(object sender, System.Windows.RoutedEventArgs e) {
-			UClickNoFun.Open(new Uri("sonidos\\uclick_jewel.wav", UriKind.Relative));
-			// Application.Current.Dispatcher.Invoke(() => {
-			UClickNoFun.Play();
-			// });
+			//Es muy molesto.
+			// Sonidito.Open(new Uri("sonidos\\uclicknofun.wav", UriKind.Relative));
+			Sonidito.Play();
 		}
 
 		static public void PlayClickJewel() {
-			UClickNoFun.Open(new Uri("sonidos\\uclick_jewel.wav", UriKind.Relative));
-			UClickNoFun.Play();
+			Sonidito.Open(new Uri("sonidos\\uclick_jewel.wav", UriKind.Relative));
+			Sonidito.Play();
 		}
 		
 		
